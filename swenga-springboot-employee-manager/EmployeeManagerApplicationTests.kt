b9605a35eafc04b85798d805cf267c180c31c527
Dominik Kainz <73217569+domiK66@users.noreply.github.com>
2022-01-11 12:43:35 +0100

    Update EmployeeManagerApplicationTests.kt

--- swenga-springboot-employee-manager/EmployeeManagerApplicationTests.kt
+++ swenga-springboot-employee-manager/EmployeeManagerApplicationTests.kt
@@ -1,6 +1,7 @@
 package at.fhj.ima.employee.employeemanager
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class EmployeeManagerApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		Assertions.assertEquals(1, 1)
 	}
 
 }
