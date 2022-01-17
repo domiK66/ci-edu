83e9a0aa1abdd48f0d5ded40b67d67fe25742e57
Dominik Kainz <73217569+domiK66@users.noreply.github.com>
2022-01-17 14:36:40 +0100

    Update EmployeeManagerApplicationTests.kt

--- swenga-springboot-employee-manager/EmployeeManagerApplicationTests.kt
+++ swenga-springboot-employee-manager/EmployeeManagerApplicationTests.kt
@@ -1,14 +1,30 @@
 package at.fhj.ima.employee.employeemanager
 
+import at.fhj.ima.employee.employeemanager.repository.DepartmentRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-
-class EmployeeManagerApplicationTests {
+@SpringBootTest(
+	classes = [EmployeeManagerApplication::class],
+	webEnvironment = WebEnvironment.RANDOM_PORT
+)
+internal class EmployeeManagerApplicationTests {
 
 	@Test
-	fun contextLoads() {
-		Assertions.assertEquals(1, 1)
+	fun sampleUnitTest() {
+		Assertions.assertEquals(1,1)
+
+	}
+
+	@Autowired
+	lateinit var departmentRepository: DepartmentRepository
+
+	@Test
+	fun sampleIntegrationTest() {
+		Assertions.assertEquals(departmentRepository.findAll().size, 3)
 	}
 
 }
